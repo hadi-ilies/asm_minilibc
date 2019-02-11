e6d782e26a0f1fee41cdf57803454face3b84aac
hadi-ilies.bereksi-reguig <hadibereksi@hadi-ilies.bereksi-reguig>
2019-02-11 11:23:53 +0100

    strlen

---
+++ src/strlen.s
@@ -0,0 +1,16 @@
+    global strlen:function
+    section .text
+
+
+strlen:
+    xor rax,  rax    ;; set counter to 0
+    
+loop_strlen:
+    cmp  BYTE[rdi],  0  ; get next character
+    jz   Finished            ; CL == 0, so exit the loop
+    inc  rax                 ; increment pointer 
+    inc rdi                   ; increment rdi move pointer on string
+    jmp loop_strlen
+    
+Finished:
+    ret
