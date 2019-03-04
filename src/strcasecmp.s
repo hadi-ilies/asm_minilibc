acd04b0cfcef83b7e574eeaf947b6ca438599a88
Parallels <hadi-ilies.bereksi-reguig@epitech.eu>
2019-03-04 11:37:18 +0100

    strcasecmp fixed

--- src/strcasecmp.s
+++ src/strcasecmp.s
@@ -7,26 +7,26 @@ strcasecmp:
     xor r9, r9
     
 Loop:
-   cmp BYTE[rdi], 0
-   jz Diff
    mov r8b, BYTE[rdi]
-   cmp r8b, 90 ;; if (r8b > Z)
+   cmp r8b, 91 ;; if (r8b >= Z + 1)
    jns Loop1
-   cmp r8b,  65 ;; if(r8b < A)
+   cmp r8b, 65 ;; if(r8b < A)
    js Loop1
    add r8b, 32 ;;convert to lower case
 
 Loop1:
    mov r9b, BYTE[rsi]
-   cmp r9b, 90 ;; if (r8b > Z)
+   cmp r9b, 91 ;; if (r8b >= Z + 1)
    jns Comp1
-   cmp r9b,  65 ;; if(r8b < A)
+   cmp r9b, 65 ;; if(r8b < A)
    js Comp1
    add r9b, 32 ;; convert to lower case
 
 Comp1:
    cmp r8b, r9b
    jnz Diff
+   cmp BYTE[rdi], 0
+   jz Diff	
    inc rdi
    inc rsi
    jmp Loop
