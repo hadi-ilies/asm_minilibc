82c5ddd5ee252d3d9dad3b34a1c68529d596fa12
Parallels <hadi-ilies.bereksi-reguig@epitech.eu>
2019-10-12 01:32:01 +0200

    commit some register, too tired to continue

--- src/memcpy.s
+++ src/memcpy.s
@@ -1,16 +1,18 @@
-  global        memcpy:function
-                    ;;rdi -> first parameter | rsi -> second parameter when  equal to type *str | rdx | rcx
+    global	memcpy:function
+	
+;;rdi -> first parameter | rsi -> second parameter when  equal to type *str | rdx | rcx
+
 memcpy:
-    mov rax, rdi
+    mov 	rax, rdi 	;init rax with rdi
 
 loop_memcpy:
-    cmp rdx, 0
-    jz exit
-    mov al, BYTE[rsi]
-    mov BYTE[rdi], al
-    dec rdx
-    inc rdi
-    inc rsi
-    jmp loop_memcpy
+    cmp 	rdx, 0		;compare rdx with 0
+    jz		exit
+    mov		al, BYTE[rsi]	;move byte in rsi into al 
+    mov 	BYTE[rdi], al	;move al into rdi
+    dec 	rdx		;decrement rdx
+    inc 	rdi		;increment rdx
+    inc 	rsi		;increment rsi
+    jmp 	loop_memcpy	;kind of recursion
 exit:
-    ret
+    ret				;return rax
--- src/memmove.s
+++ src/memmove.s
@@ -1,30 +1,30 @@
-  global        memmove:function
+    global	memmove:function
                     ;;rdi -> first parameter | rsi -> second parameter when  equal to type *str | rdx | rcx
 
 memmove:
-	cmp	rsi, rdi
-	jl	memcpy
-	mov rax, rdi
-	mov rcx, rdx
-	mov rdx, rsi
-	cld 			;clear direction flag left to right
-	rep movsb		;rep movsp repete action in parameter
-	mov rsi, rdx
-	mov rdi, rax
+    cmp		rsi, rdi	;compare rsi with rdi
+    jl		memcpy		;call memcpy
+    mov 	rax, rdi
+    mov 	rcx, rdx
+    mov 	rdx, rsi
+    cld 			;clear direction flag left to right
+    rep 	movsb		;rep movsp repete action in parameter
+    mov 	rsi, rdx
+    mov 	rdi, rax
     ret
 
 memcpy:
-    mov rax, rdi
+    mov 	rax, rdi
 
 loop_memcpy:
-    cmp rdx, 0
-    jz exit
-    mov al, BYTE[rsi]
-    mov BYTE[rdi], al
-    dec rdx
-    inc rdi
-    inc rsi
-    jmp loop_memcpy
+    cmp 	rdx, 0
+    jz 		exit
+    mov 	al, BYTE[rsi]
+    mov 	BYTE[rdi], al
+    dec 	rdx
+    inc 	rdi
+    inc 	rsi
+    jmp 	loop_memcpy
 
 exit:
     ret
