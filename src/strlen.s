d79569d31672055b8a05456b2355017e27f6d057
Parallels <hadi-ilies.bereksi-reguig@epitech.eu>
2019-10-12 01:18:28 +0200

    have to add commit

--- src/strlen.s
+++ src/strlen.s
@@ -2,13 +2,13 @@
     section .text
 
 strlen:
-    xor rax,  rax    ;; set counter to 0
+    xor rax,  rax    ; set counter to 0
     
 loop_strlen:
-    cmp  BYTE[rdi],  0  ; get next character
-    jz   Finished            ; CL == 0, so exit the loop
-    inc  rax                 ; increment pointer 
-    inc rdi                   ; increment rdi move pointer on string
+    cmp  BYTE[rdi],  0	; get next character
+    jz   Finished	; CL == 0, so exit the loop
+    inc  rax		; increment pointer 
+    inc rdi		; increment rdi move pointer in string
     jmp loop_strlen
     
 Finished:
