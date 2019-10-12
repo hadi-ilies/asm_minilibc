d103c9e985a7cab2b8e746bbf90f3155c43610e4
Parallels <hadi-ilies.bereksi-reguig@epitech.eu>
2019-10-12 09:52:07 +0200

    Commit rindex and strchr

--- src/rindex.s
+++ src/rindex.s
@@ -1,16 +1,17 @@
-    global rindex:function
-    section .text
+    global 	rindex:function
+    section 	.text
 
 rindex:
-	xor rax, rax ;; set value to 0
+    xor 	rax, rax 	;; set value to 0
 
 loop_rindex:
-    cmp BYTE[rdi], sil
-    cmove rax, rdi
-    cmp BYTE[rdi], 0
-    jz Finished
-    inc rdi
-    jmp loop_rindex
+    cmp 	BYTE[rdi], sil	;; compare byte rdi with sil register
+    cmove 	rax, rdi	;; move rdi in rax
+    cmp 	BYTE[rdi], 0	;; compare byte rdi with 0
+    jz 		Finished	;; true go to Finished func
+    inc 	rdi		;; increment rdi
+    jmp 	loop_rindex	;; recursion with loop_rindex
 
 Finished:
-    ret
+    ret				;; return rax register
+
--- src/strchr.s
+++ src/strchr.s
@@ -1,20 +1,20 @@
-    global strchr:function
-    section .text
+    global 	strchr:function
+    section 	.text
 
 strchr:
-	xor rax, rax ;; set value to first rdi
-	cmp rdi, 0
-	jz Error
+    xor 	rax, rax 	;; set value to 0;; 
+    cmp 	rdi, 0		;; compare rdi to 0 
+    jz 		Error 		;; true so go to Error func
 
 loop_strchr:
-	cmp BYTE[rdi], sil ;; sil its the second parameter
-	jz Finished
-	cmp BYTE[rdi], 0
-	jz Error
-	inc rdi
-	jmp loop_strchr
+    cmp 	BYTE[rdi], sil 	;; sil its the second parameter
+    jz 		Finished	;; true so go to finish func 
+    cmp 	BYTE[rdi], 0	;; compare byte of rdi with 0
+    jz 		Error		;; true so go to Error func
+    inc 	rdi		;; increment rdi
+    jmp 	loop_strchr	;; recursion in loop_strchr
 Error:
-	ret
+	ret			;; return rax register
 Finished:
-	mov rax, rdi
-	ret
+	mov 	rax, rdi	;; move rdi in rax
+	ret			;; return rax register
